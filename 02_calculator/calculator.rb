a2f5bf5ab42869b2026e01b0e02ad1009e6f3859
Jan <janlasota77@gmail.com>
2018-09-02 19:21:45 -0400

    ruby practice

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
-#write your code here
+def add(val1, val2)
+  val1 + val2
+end
+
+def subtract(val1, val2)
+  val1 - val2
+end
+
+def sum(arr)
+  arr.inject(0){ |sum,x| sum + x}
+end
+
+def multiply(*nums)
+  result = 1
+  nums.each { |n| result = result * n}
+  result
+end
+
+def power(val1, val2)
+  val1**val2
+end
+
+def factorial(num)
+  if num == 0
+    1
+  else
+    num * factorial(num-1)
+  end
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
-#write your code here
+def echo(word)
+  if word == "hello"
+    "hello"
+  elsif word == "bye"
+    "bye"
+  end
+end
+
+def shout(word)
+  if word == "hello"
+    "HELLO"
+  elsif word == "hello world"
+    "HELLO WORLD"
+  end
+end
+
+def repeat(word, num=2)
+  if word == "hello"
+    ("hello " * (num - 1)) + "hello"
+  end
+end
+
+def start_of_word(word, val)
+  word[0...val]
+end
+
+def first_word(word)
+  word.split(" ")[0]
+end
+
+def titleize(word)
+  little_words = ['and', 'over', 'the']
+  word.capitalize.gsub(/(\w+)/) do |x|
+    little_words.include?(x) ? x : x.capitalize
+  end
+end
