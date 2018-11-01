8b90b8c69af6deee4aa9f228fe88c1afb076b0ac
Jan <janlasota77@gmail.com>
2018-11-01 14:34:19 -0400

    more practice

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,9 @@
-#write your code here
+def translate(word)
+  if word.start_with?("a", "e", "i", "o", "u")
+    word + "ay"
+  elsif word.start_with?("b")
+    word[1..-1] + word[0..0] + "ay"
+  elsif word.start_with?("ch")
+    word[2..-1] + word[0..1] + "ay"
+  end
+end
